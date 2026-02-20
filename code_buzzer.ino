25081681c3e14964179a0043f55e7fa8c274892a
YasmineELHASSANI <yasmine.elhassani@etu.unilasalle.fr>
2026-02-20 12:11:57 +0100

    Implement melody playback for buzzer
    Added melody arrays and implemented tone playback in loop.

--- code_buzzer.ino
+++ code_buzzer.ino
@@ -11,3 +11,29 @@ void loop() {
   digitalWrite(BUZZER_PIN, LOW);  // Buzzer OFF
   delay(500);
 }
+
+
+//melody bonus qui sert pas
+#define BUZZER_PIN 8
+
+int melody1[] = {392, 349, 392, 330};
+int melody2[]={494, 440, 494, 392};
+int duration = 250;
+
+void setup() {
+}
+
+void loop() {
+  for (int i=0; i<8; i++){
+    for (int j = 0; j<4; j++){
+      tone(BUZZER_PIN, melody1[j]);
+      delay(duration);
+    }
+    for (int k = 0; k<4; k++){
+      tone(BUZZER_PIN, melody2[k]);
+      delay(duration);
+    }
+  }
+  noTone(BUZZER_PIN);
+  delay(1000);
+}
