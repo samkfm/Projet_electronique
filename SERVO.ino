67e9b353d75e95532cc1af4e327332c8efd79ed1
YasmineELHASSANI <yasmine.elhassani@etu.unilasalle.fr>
2026-04-29 13:57:14 +0200

    Update servo control logic and comments
    Added comments for turning right and modified backward movement logic.

--- SERVO.ino
+++ SERVO.ino
@@ -14,19 +14,25 @@ void setup() {
 }
 
 void loop() {
+  /**
   // AVANCER OK !!
   gauche.write(0);
   droite.write(180); 
   delay(1000);
-/**
+
   // TOURNER À GAUCHE (Les deux roues vers l'arrière ou une à l'arrêt)
   gauche.write(90); // Arrêt roue gauche
   droite.write(0);  // Marche avant roue droite
   delay(1000);
-
+**/
+  // TOURNER À DROITE (Les deux roues vers l'arrière ou une à l'arrêt)
+  gauche.write(0); // Arrêt roue gauche
+  droite.write(180);  // Marche avant roue droite
+  delay(1000);
+/**
   // RECULER
-  gauche.write(0);
-  droite.write(180);
+  gauche.write(180);
+  droite.write(0);
   delay(1000);
   
   // ARRÊT
